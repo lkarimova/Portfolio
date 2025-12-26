fdbb67dc0827965d0993f415e35f1c8873eaf765
lkarimova <148022557+lkarimova@users.noreply.github.com>
2025-12-26 18:00:36 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/',
+    base: '/Portfolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
